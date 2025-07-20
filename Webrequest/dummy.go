f88652608d0b3941224c89d4cf097e31432fdea4
sabarim6369 <sabarim6369@gmail.com>
2025-07-20 22:50:48 +0530

    unmarshal

--- Webrequest/dummy.go
+++ Webrequest/dummy.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
+type Data struct{
+	Userid int    `json:"userId"`
+	Postid int    `json:"id"`
+	Title string    `json:"title"`
+	Body string     `json:"body"`
 
+}
 func Dummy() {
 	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
 	errorhandler(err)
@@ -13,9 +20,13 @@ func Dummy() {
 
 	data, err := io.ReadAll(resp.Body)
 	errorhandler(err)
-	
-	content := string(data)
-	fmt.Println(content)
+
+	// content := string(data)
+	var postdata Data;
+	err=json.Unmarshal(data,&postdata);
+	errorhandler(err);
+fmt.Printf("%+v\n", postdata)
+	// fmt.Println(content)
 }
 
 func errorhandler(err error) {
